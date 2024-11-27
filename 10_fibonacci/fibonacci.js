39173cdc5c4f1f9afee9a2adaf3d58f0d9014c95
Kieran Gough <187461911+Kieran-Go@users.noreply.github.com>
2024-11-27 14:49:12 +1030

    Complete exercise 08 through 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
 
+    // Convert to number if string
+    let num;
+    if(typeof numArg !== 'number'){
+        num = parseInt(numArg);
+    }
+    else{
+        num = numArg;
+    }
+
+    // Doesn't accept negatives
+    if(num < 0) return "OOPS";
+    // 0th fib is 0
+    if(num === 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= num; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
